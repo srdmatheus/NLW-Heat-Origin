64eacfc0b7d166b3a3d2ff6eeccf4c9b5398b0dc
Matheus Ribeiro <srdmatheus@gmail.com>
2021-10-21 14:30:59 -0300

    update css

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
+/*const getName = prompt('Qual o seu nome?')
+let getGitHub = prompt('Perfil do github')
+let getYoutube = prompt('Perfil do youtube', getGitHub)
+let getFacebook = prompt('Perfil do facebook', getYoutube)
+let getInstagram = prompt('Perfil do instagram', getFacebook)
+let getTwitter = prompt('Perfil do twitter', getInstagram)*/
+
 const linksSocialMedia = {
-  github: 'srdmatheus',
-  youtube: 'srdmatheus',
-  facebook: 'srdmatheus',
-  instagram: 'srdmatheus',
-  twitter: 'srdmatheus'
+  github: getGitHub,
+  youtube: getYoutube,
+  facebook: getFacebook,
+  instagram: getInstagram,
+  twitter: getTwitter
 }
 
 function changeSocialMedia() {
-  userName.textContent = 'Mateus Ribeiro'
+  userName.textContent = getName
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
     li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
   }
 }
 changeSocialMedia()
+
+function changeUserGitHub() {
+  userGithub.textContent = getGitHub
+}
+changeUserGitHub()
+
+function setAvatar() {
+  userAvatar.src = `https://github.com/${getGitHub}.png`
+}
+setAvatar()
