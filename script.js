2fd8b003c2e26e8f6a2e2e6db9a7670c812ceb7b
srdmatheus <srdmatheus@gmail.com>
2021-10-23 17:39:52 -0300

    added modal for inputs

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
 const Modal = {
   close() {
-    // fechar o modal
-    // remover a class active do modal
     document.querySelector('.modal-overlay').classList.remove('active')
   }
 }
+const submit = document.querySelector('#submit')
 
-//let getGitHub = prompt('github.com/')
-//let getYoutube = prompt('youtube.com/', getGitHub)
-//let getFacebook = prompt('facebook.com/', getYoutube)
-//let getInstagram = prompt('instagram.com/', getFacebook)
-//let getTwitter = prompt('twitter.com/', getInstagram)
+submit.addEventListener('click', function (e) {
+  e.preventDefault()
+  let getGitHub = document.querySelector('#getGitHub').value
+  let getYoutube = document.querySelector('#getYoutube').value
+  let getFacebook = document.querySelector('#getFacebook').value
+  let getInstagram = document.querySelector('#getInstagram').value
+  let getTwitter = document.querySelector('#getTwitter').value
 
-const linksSocialMedia = {
-  github: getGitHub,
-  youtube: getYoutube,
-  facebook: getFacebook,
-  instagram: getInstagram,
-  twitter: getTwitter
-}
-
-function changeSocialMediaLinks() {
-  for (let li of socialLinks.children) {
-    const social = li.getAttribute('class')
-    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  const linksSocialMedia = {
+    github: getGitHub,
+    youtube: getYoutube,
+    facebook: getFacebook,
+    instagram: getInstagram,
+    twitter: getTwitter
   }
-}
-changeSocialMediaLinks()
 
-function getGithHubProfileInfos() {
-  const url = `https://api.github.com/users/${getGitHub}`
+  function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+      const social = li.getAttribute('class')
+      li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    }
+  }
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      userName.textContent = data.name
-      usernameGitHub.textContent = data.login
-      userLink.href = data.html_url
-      document.title = `${data.name} - DoWhile 2021`
-      userAvatar.src = data.avatar_url
-    })
-}
-getGithHubProfileInfos()
+  function getGithHubProfileInfos() {
+    const url = `https://api.github.com/users/${getGitHub}`
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        userName.textContent = data.name
+        usernameGitHub.textContent = data.login
+        userLink.href = data.html_url
+        document.title = `${data.name} - DoWhile 2021`
+        userAvatar.src = data.avatar_url
+      })
+  }
+
+  changeSocialMediaLinks()
+  getGithHubProfileInfos()
+})
